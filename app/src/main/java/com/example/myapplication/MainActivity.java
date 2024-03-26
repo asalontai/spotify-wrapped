d34c6c9fcca993d9e28674e9f040172ca1178660
Krish-Mathur <krishmathur777@gmail.com>
2024-03-26 13:51:48 -0400

    first changes
    pushing spotify api setup + login/auth functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String REDIRECT_URI = "SPOTIFY-SDK://auth";
+    //public static final String REDIRECT_URI = "SPOTIFY-SDK://auth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginButtonClick(View view) {
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
     }
 }
