2c839fd1c6b64e6e242a6ed15afacd1cc334c7c8
Tiga1423 <108031405+Tiga1423@users.noreply.github.com>
2024-04-04 22:02:23 -0400

    some ui stuff

--- app/src/main/java/com/example/myapplication/LoggedInActivity.java
+++ app/src/main/java/com/example/myapplication/LoggedInActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoggedInActivity extends AppCompatActivity {
+    private Button settingsBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+        settingsBtn = findViewById(R.id.accountBtn);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoggedInActivity.this, MyWrapsActivity.class);
+                startActivity(intent);
+            }
+        });
         // Add any initialization or setup code for your logged-in view
     }
     public void logoutOnClick(View view) {
---
+++ app/src/main/java/com/example/myapplication/MyWrapsActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MyWrapsActivity extends AppCompatActivity {
+    private Button toMyAccountBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_wraps);
+        toMyAccountBtn = findViewById(R.id.MyAccountBtn);
+        toMyAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyWrapsActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
