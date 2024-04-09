ab6ea6c380860f72bf24a92576fedce0a2b77254
alexsalontai <afsalontai@gmail.com>
2024-04-09 16:39:31 -0400

    added wraps

--- app/src/main/java/com/example/myapplication/MyWrapsActivity.java
+++ app/src/main/java/com/example/myapplication/MyWrapsActivity.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -29,13 +30,14 @@ public class MyWrapsActivity extends AppCompatActivity {
 
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private Call mCall;
-    private String mAccessToken = AuthActivity.accessToken;
     private String mAccessCode = AuthActivity.accessCode;
     private ImageView image1, image2, image3, image4, image5;
     private TextView text1, text2, text3, text4, text5;
     private String[] artistNames = new String[5];
     private String[] imageUrls = new String[5];
     private Button toMyAccountBtn;
+
+    private Button myWrapsBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,21 +63,32 @@ public class MyWrapsActivity extends AppCompatActivity {
         image4 = (ImageView) findViewById(R.id.image4);
         image5 = (ImageView) findViewById(R.id.image5);
 
-        getWraps();
+        Button wrapsBtn = (Button) findViewById(R.id.generateWrapsBtn);
+        wrapsBtn.setOnClickListener((v) -> {
+            getWraps();
+        });
+
 }
 
     private void getWraps() {
         if (AuthActivity.accessToken == null) {
             Toast.makeText(this, "You need to get an access token first!", Toast.LENGTH_SHORT).show();
             return;
+        } else {
+            Log.d("Token", AuthActivity.accessToken);
         }
-
 
         // Create a request to get the user profile
         final Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/me/top/artists?limit=5&offset=0")
-                .addHeader("Authorization", "Bearer " + mAccessToken)
+                .url("https://api.spotify.com/v1/me/top/artists?limit=5")
+                .addHeader("Authorization", "Bearer " + AuthActivity.accessToken)
                 .build();
+
+        Headers headers = request.headers();
+        for (String name : headers.names()) {
+            Log.d("Request Header", name + ": " + headers.get(name));
+        }
+
 
         cancelCall();
         mCall = mOkHttpClient.newCall(request);
@@ -90,44 +103,49 @@ public class MyWrapsActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    String responseData = response.body().string();
-                    JSONObject jsonResponse = new JSONObject(responseData);
-                    JSONArray artistsArray = jsonResponse.getJSONArray("items");
+                if (response.isSuccessful()) {
+                    final String responseData = response.body().string();
+                    Log.d("API response", responseData);
+                    try {
+                        JSONObject jsonObject = new JSONObject(responseData);
+                        JSONArray items = jsonObject.getJSONArray("items");
 
-                    for (int i = 0; i < artistsArray.length(); i++) {
-                        JSONObject artist = artistsArray.getJSONObject(i);
-                        artistNames[i] = artist.getString("name");
-                        imageUrls[i] = artist.getJSONArray("images").getJSONObject(0).getString("url");
-                    }
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            // Set artist names to TextViews
+                        for (int i = 0; i < items.length(); i++) {
+                            JSONObject item = items.getJSONObject(i);
+                            String artistName = item.getString("name");
+                            artistNames[i] = artistName;
+
+                            JSONArray images = item.getJSONArray("images");
+                            JSONObject image = images.getJSONObject(0); // Assuming the first image is the one to be displayed
+                            String imageUrl = image.getString("url");
+                            imageUrls[i] = imageUrl;
+                        }
+
+                        // Update UI on the main thread
+                        runOnUiThread(() -> {
+                            // Set text and load images into ImageView
                             text1.setText(artistNames[0]);
                             text2.setText(artistNames[1]);
                             text3.setText(artistNames[2]);
                             text4.setText(artistNames[3]);
                             text5.setText(artistNames[4]);
 
-                            // Load images into ImageViews
                             loadImage(imageUrls[0], image1);
                             loadImage(imageUrls[1], image2);
                             loadImage(imageUrls[2], image3);
                             loadImage(imageUrls[3], image4);
                             loadImage(imageUrls[4], image5);
-                        }
-                    });
-                } catch (JSONException e) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MyWrapsActivity.this, "Failed to fetch data, watch Logcat for more details", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    Log.d("HTTP", "Failed to fetch data: " + e);
+                        });
+                    } catch (JSONException e) {
+                        Log.d("JSON", "Failed to parse data: " + e);
+                        runOnUiThread(() -> Toast.makeText(MyWrapsActivity.this, "Failed to parse data", Toast.LENGTH_SHORT).show());
+                    }
+                } else {
+                    // Handle unsuccessful response
+                    runOnUiThread(() -> Toast.makeText(MyWrapsActivity.this, "Unsuccessful response", Toast.LENGTH_SHORT).show());
                 }
             }
+
         });
     }
 
